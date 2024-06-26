c1f1bf289879d9c8aaff8114ef665e88bb3ee9db
Daniel <hd2242kin20@gmail.com>
2024-06-25 23:23:17 -0300

    2

---
+++ react-tcc/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="background-image"></div>
+            <div className="card-container">
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
