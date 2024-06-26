e7d51c2dd5cc9ae5e83e7ffae4ea05b09c9aed79
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-26 07:42:50 -0300

    4

--- react-tcc/src/App.jsx
+++ react-tcc/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import Footer from "./Componentes/Footer/Footer";
+import Header from "./Componentes/Header/Header";
+import { Outlet } from "react-router-dom"
 
 function App() {
-
   return (
     <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
-
-export default App
+export default App; 
